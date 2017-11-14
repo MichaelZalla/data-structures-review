97af16b54ca107170a0b28489d2d2358d332e44a
Michael Zalla <michael@zalla.io>
2017-11-13 23:10:01 -0800

    Add simple bucket-sort implementation;

---
+++ src/28-bucket-sort/cpp/BucketSort.h
@@ -0,0 +1,61 @@
+#ifndef BUCKET_SORT_H
+#define BUCKET_SORT_H
+
+#include <vector>
+
+#include "../../assert.h"
+
+typedef std::vector<int> Bucket;
+
+void bucketSort(
+	std::vector<int>& collection)
+{
+
+
+	int size = collection.size();
+
+	assert("Collection is not empty.", size > 0);
+
+	if(size < 2)
+	{
+		return;
+	}
+
+	std::vector<Bucket> buckets(size);
+
+	for(
+		std::vector<int>::iterator pos = collection.begin();
+		pos != collection.end();
+		++pos)
+	{
+		int key = *pos;
+
+		buckets[key].push_back(key);
+	}
+
+	collection.clear();
+
+	assert("Some bucket lists have been populated.", buckets.size() > 0);
+
+	for(
+		std::vector<Bucket>::iterator bucketPos = buckets.begin();
+		bucketPos != buckets.end();
+		++bucketPos)
+	{
+		Bucket bucket = *bucketPos;
+
+		for(
+			Bucket::iterator entryPos = bucket.begin();
+			entryPos != bucket.end();
+			++entryPos)
+		{
+			collection.push_back(*entryPos);
+		}
+
+	}
+
+	return;
+
+}
+
+#endif
---
+++ src/28-bucket-sort/cpp/main.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+#include "../../assert.h"
+#include "../../07-vector/PrintVector.h"
+
+#include "./BucketSort.h"
+
+int main(int argc, char** argv)
+{
+
+	int unsorted[] = { 5,5,1,2,4,5,1,4,2,4,2,3,1,2,4,5,1 };
+	int sorted[] = { 1,1,1,1,2,2,2,2,3,4,4,4,4,5,5,5,5 };
+	int n = sizeof(unsorted)/sizeof(int);
+
+	std::vector<int> keys(unsorted, unsorted + n);
+
+	bucketSort(keys);
+
+	assert("Bucket sort", keys == std::vector<int>(sorted, sorted + n));
+
+	return 0;
+
+}
